35d469f2885f6d7b622643258c9142d0b5e300e1
Praveen Tamboli <praveentamboli55@gmail.com>
2025-07-19 18:15:57 +0530

    Chapter 1 var let const and undefined

---
+++ Chapter_1_var_let const_undefined.js
@@ -0,0 +1,15 @@
+//  var let const and undefined
+
+// const // Cannot be changed
+// let // can be changed
+// var //can be changed
+
+accountName // if we define a variable without any of the 
+            // above keyword then also js does not gives issue
+            
+// NOTE
+/* var should not be used because 
+it has problem with functional and block scope.
+*/
+
+// let newName; //when we will print this it will give undefined.
